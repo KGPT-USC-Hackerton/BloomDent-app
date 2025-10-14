ae689f5ff83c0cc87d0ef925a2d4879ef271fce0
Lim kyungmin <km8192@naver.com>
2025-10-15 02:04:03 +0900

    refactor: 탭바 백터 아이콘 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView, StatusBar, Platform } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import HomeScreen from './screens/HomeScreen';
 import CareScreen from './screens/CareScreen';
 import AppointmentScreen from './screens/AppointmentScreen';
@@ -11,7 +12,8 @@ const Tab = createBottomTabNavigator();
 
 function App() {
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
+      <SafeAreaView style={styles.topSafeArea} />
       <NavigationContainer>
         <Tab.Navigator 
           initialRouteName="Home"
@@ -19,19 +21,58 @@ function App() {
             headerShown: false
           }}
         >
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Care" component={CareScreen} />
-          <Tab.Screen name="Appointment" component={AppointmentScreen} />
-          <Tab.Screen name="MyPage" component={MyPageScreen} />
+          <Tab.Screen 
+            name="Home" 
+            component={HomeScreen} 
+            options={{
+              title: '홈',
+              tabBarIcon: ({color, size}) => (
+                <Icon name="home" color={color} size={size} />
+              ),
+            }} />
+          <Tab.Screen 
+            name="Care"
+            component={CareScreen}
+            options={{
+              title: '관리',
+              tabBarIcon: ({color, size}) => (
+                <Icon name="camera" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Appointment"
+            component={AppointmentScreen}
+            options={{
+              title: '예약',
+              tabBarIcon: ({color, size}) => (
+                <Icon name="calendar-month" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="My Page"
+            component={MyPageScreen}
+            options={{
+              title: '마이페이지',
+              tabBarIcon: ({color, size}) => (
+                <Icon name="person" color={color} size={size} />
+              ),
+            }}
+          />
         </Tab.Navigator>
       </NavigationContainer>
-    </SafeAreaView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  topSafeArea: {
+    flex: 0,
+    backgroundColor: '#f8f9fa',
   },
 });
 
