6344d3c6707216cb7f66c386d9d5a16544f66a57
Lim kyungmin <km8192@naver.com>
2025-10-15 00:41:51 +0900

    refactor: 모든 페이지 MVP 스타일로 추가

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen';
 import CareScreen from './screens/CareScreen';
+import AppointmentScreen from './screens/AppointmentScreen';
+import MyPageScreen from './screens/MyPageScreen';
+
 const Tab = createBottomTabNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator initialRouteName="Home">
-	      <Tab.Screen name="Home" component={HomeScreen} />
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <Tab.Navigator 
+          initialRouteName="Home"
+          screenOptions={{
+            headerShown: false
+          }}
+        >
+          <Tab.Screen name="Home" component={HomeScreen} />
           <Tab.Screen name="Care" component={CareScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+          <Tab.Screen name="Appointment" component={AppointmentScreen} />
+          <Tab.Screen name="MyPage" component={MyPageScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
